04ed957993cb734c854c4dde5ee0e60502b7dafd
DieLoves <insar667@gmail.com>
2025-05-20 09:40:25 +0500

    Enhance Generics section in README and update related TypeScript examples for clarity

--- generics.ts
+++ generics.ts
@@ -8,29 +8,88 @@ interface Article {
 	description: string;
 }
 
-// T - общепринятая конвенция
-interface ApiResponse<Data, Meta> {
-	status?: 'error' | 'success'; // Литералы
-	meta?: Meta;
-	requestId?: string;
+interface Tree<T> {
+	id: string;
+	value: T;
+	children: Tree<T>[] | null;
+}
+
+const treeNode: Tree<PlayerData> = {
+	id: '10',
+	value: {
+		username: 'Robert',
+	},
+	children: [
+		{
+			id: '20',
+			value: {
+				username: 'Vasya',
+			},
+			children: null,
+		},
+		{
+			id: '30',
+			value: {
+				username: 'Masha',
+			},
+			children: null,
+		},
+	],
+};
+
+function genericFn<T>(arg: T) {}
+
+// Ограничения в Generic
+function createEntity<T extends { id: string; createdAt: Date }>(arg: T) {}
+
+// Type 'PlayerData' does not satisfy the constraint '{ id: string; createdAt: Date; }'.
+// createEntity<PlayerData>({ username: 'Vasya' });
+
+const arrowGeneric = <T>(arg: T): T => {
+	return arg;
+};
+
+const data = arrowGeneric<PlayerData>({ username: 'Robert' });
+
+// class Order<T> {
+// 	data: T;
+
+// 	constructor(arg: T) {
+// 		this.data = arg;
+// 	}
+// }
+
+interface ApiResponse<Data = string> {
 	data: Data;
 }
 
-// PlayerData прокидывается в ApiResponse. data: T -> PlayerData
-const response: ApiResponse<PlayerData, Metadata> = {
-	data: {
-		username: 'Aboba',
-	},
+const response: ApiResponse = {
+	data: 'ok',
 };
 
-// { timestamp: number } можно не выносить, если мы его не переиспользуем
-// const responseArticle: ApiResponse<Article, { timestamp: number }> = {
-interface MetaData {
-	timestamp: number;
-}
-const responseArticle: ApiResponse<Article, MetaData> = {
-	data: {
-		title: '123',
-		description: '456',
-	},
-};
+// // T - общепринятая конвенция
+// interface ApiResponse<Data, Meta> {
+// 	status?: 'error' | 'success'; // Литералы
+// 	meta?: Meta;
+// 	requestId?: string;
+// 	data: Data;
+// }
+
+// // PlayerData прокидывается в ApiResponse. data: T -> PlayerData
+// const response: ApiResponse<PlayerData, Metadata> = {
+// 	data: {
+// 		username: 'Aboba',
+// 	},
+// };
+
+// // { timestamp: number } можно не выносить, если мы его не переиспользуем
+// // const responseArticle: ApiResponse<Article, { timestamp: number }> = {
+// interface MetaData {
+// 	timestamp: number;
+// }
+// const responseArticle: ApiResponse<Article, MetaData> = {
+// 	data: {
+// 		title: '123',
+// 		description: '456',
+// 	},
+// };
